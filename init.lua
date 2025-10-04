78cbcaa551ff15d8cc0d793242aa1d912decfc55
Marcel Mork <marcel.mork@posteo.de>
2025-10-04 18:45:30 +0200

    Improve modus switch between coding and writing

--- init.lua
+++ init.lua
@@ -1,57 +1,78 @@
--- set leader key
+-- Set leader key
 vim.g.mapleader = " "
 
--- load lazy.nvim
+-- Load lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath})
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- load plugins from the 'plugins' folder
+-- Load plugins from 'plugins' folder
 require("lazy").setup("plugins")
 
--- general neovim options
+-- General Neovim options
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 -- Automatic theme switching based on system mode
 local function set_theme()
-  local is_dark = vim.fn.has('night_mode') == 1
+  local is_dark = vim.fn.has("night_mode") == 1
   if is_dark then
-    vim.cmd('colorscheme catppuccin-mocha')
+    vim.cmd("colorscheme catppuccin-mocha")
   else
-    vim.cmd('colorscheme catppuccin-latte')
+    vim.cmd("colorscheme catppuccin-latte")
   end
 end
 set_theme()
 
--- Start Neovim in my writing folder
+-- Start Neovim in writing folder (optional)
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argc() == 0 then
-      if pcall(require, "nvim-tree") then
-          vim.cmd('NvimTreeToggle ' .. vim.fn.expand('~/Dokumente/Schreiben/'))
+      local ok, _ = pcall(require, "nvim-tree")
+      if ok then
+        vim.cmd("NvimTreeOpen " .. vim.fn.expand("~/Dokumente/Schreiben"))
       end
     end
   end,
-  once = true
+  once = true,
 })
-vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle File/Vault Tree' })
 
-local writing_path = vim.fn.expand('~/Dokumente/Schreiben')
-local coding_path = vim.fn.expand('~/repos')
+vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Tree" })
 
-vim.keymap.set('n', '<leader>ws', function()
-  vim.cmd('cd ' .. writing_path)
-  vim.cmd('NvimTreeToggle' .. writing_path)
-end, { desc = 'Schreiben' })
+-- Define workspace paths
+local writing_path = vim.fn.expand("~/Dokumente/Schreiben")
+local coding_path = vim.fn.expand("~/repos")
 
-vim.keymap.set('n', '<leader>wc', function()
-  vim.cmd('cd ' .. coding_path)
-  vim.cmd('NvimTreeToggle' .. coding_path)
-end, { desc = 'Programmieren' })
+-- Helper function to switch workspace
+local function switch_workspace(mode)
+  local path
+  if mode == "writing" then
+    path = writing_path
+  elseif mode == "coding" then
+    path = coding_path
+  else
+    print("❌ Unbekannter Modus: " .. tostring(mode))
+    return
+  end
+
+  -- Close any open NvimTree before reopening
+  vim.cmd("NvimTreeClose")
+  vim.cmd("cd " .. path)
+  vim.cmd("NvimTreeOpen " .. path)
+  print("🗂  Wechsel zu: " .. path)
+end
+
+-- Keymaps for workspaces
+vim.keymap.set("n", "<leader>ws", function() switch_workspace("writing") end,
+  { desc = "Schreibmodus" })
+vim.keymap.set("n", "<leader>wc", function() switch_workspace("coding") end,
+  { desc = "Codemodus" })
