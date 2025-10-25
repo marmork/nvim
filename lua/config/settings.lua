0679515564ec9df7aeda13ce8ae70a21730c4a96
Marcel Mork <marcel.mork@posteo.de>
2025-10-25 11:07:16 +0200

    Install gruvbox theme

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -6,14 +6,5 @@ vim.opt.mouse = "a"
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-
--- Set theme automatically
-local function set_theme()
-  local is_dark = vim.fn.has("night_mode") == 1
-  if is_dark then
-    vim.cmd("colorscheme catppuccin-mocha")
-  else
-    vim.cmd("colorscheme catppuccin-latte")
-  end
-end
-set_theme()
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "80"
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,15 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-latte' -- 'catppuccin-mocha'
+    -- Optional configuration
+    require("gruvbox").setup({
+      terminal_colors = true,  -- Apply terminal colors
+      -- italic = true,
+      -- palette_overrides = {}, -- Custom color palette
+      -- overrides = {},         -- Custom highlight group
+      theme_style = "dark",   -- "dark", "hard", "light", "soft" (defaults to "dark")
+    })
+    vim.cmd.colorscheme 'gruvbox'
   end
 }
