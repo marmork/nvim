28095980028e6f8e476686b3f65839227fb9d6f9
Marcel Mork <marcel.mork@posteo.de>
2025-10-28 10:59:46 +0100

    Add optical line break for text files

--- init.lua
+++ init.lua
@@ -17,6 +17,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "tex", "typst", "text" },
   callback = function()
     vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
   end,
 })
 
