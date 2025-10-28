1ed9d3e66f2d997e37b2f1c1cac33d82c602ccad
Marcel Mork <marcel.mork@posteo.de>
2025-10-28 11:01:24 +0100

    Remove hard line break and sort options

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,10 +1,9 @@
 -- General Neovim options
 
+vim.opt.expandtab = true
+vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.textwidth = 80
-vim.opt.colorcolumn = "80"
+vim.opt.showtabline = 2
+vim.opt.tabstop = 2
