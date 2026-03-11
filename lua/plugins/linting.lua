4d7fcf5e5e726001c2c6098f34791606299d788b
Marcel Mork <marcel.mork@posteo.de>
2026-03-11 22:56:42 +0100

    Replace event notation in linting config

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,28 +1,26 @@
 -- lua/plugins/linting.lua
 return {
-  -- 1. Formatter (conform.nvim)
-  {
-      'stevearc/conform.nvim',
-      -- Use BufWritePre to trigger the format check before writing the file.
-      event = 'BufWritePre',
-      cmd = { 'ConformInfo', 'ConformFormat' },
-      config = function()
-          -- Load the formatter setup
-          require('config.format_setup')
-      end,
-  },
+    -- 1. Formatter (conform.nvim)
+    {
+        'stevearc/conform.nvim',
+        -- Use BufWritePre to trigger the format check before writing the file.
+        event = 'BufWritePre',
+        cmd = { 'ConformInfo', 'ConformFormat' },
+        config = function()
+            -- Load the formatter setup
+            require('config.format_setup')
+        end,
+    },
 
-  -- 2. Linter (nvim-lint)
-  {
-      'mfussenegger/nvim-lint',
-      -- Load when a buffer is read or opened
-      event = 'BufReadPost',
-      config = function()
-          -- Load the linter setup
-          require('config.lint_setup')
-      end,
-  },
+    -- 2. Linter (nvim-lint)
+    {
+        'mfussenegger/nvim-lint',
+        ft = { "python", "sh", "sql" },
+        config = function()
+            require('config.lint_setup')
+        end,
+    },
   
-  -- 3. Dependency: Plenary is still useful for various plugins
-  'nvim-lua/plenary.nvim',
+    -- 3. Dependency: Plenary is still useful for various plugins
+    'nvim-lua/plenary.nvim',
 }
