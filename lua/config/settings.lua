49d0c92ed21fc1dee33e5ea75834543a5dd9b229
Marcel Mork <marcel.mork@perfact.de>
2026-03-21 15:28:07 +0100

    Deactivate relative line number

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -11,7 +11,7 @@
 vim.opt.expandtab = true
 vim.opt.mouse = "a"
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.showtabline = 2
