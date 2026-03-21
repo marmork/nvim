2d2b24c448c34ef89f46dcd31f5a4a7cc638bb4a
Marcel Mork <marcel.mork@perfact.de>
2026-03-21 11:44:18 +0100

    Fix ruff lint setup

--- lua/config/lint_setup.lua
+++ lua/config/lint_setup.lua
@@ -2,13 +2,31 @@
 
 local lint = require("lint")
 
+-- ESLint Configuration
 lint.linters.eslint.cmd = os.getenv("HOME") .. "/.npm-global/bin/eslint"
 lint.linters.eslint.args = {
   "--format", "json",
   "--config", os.getenv("HOME") .. "/.eslint.config.js",
-  vim.api.nvim_buf_get_name(0),
+  function()
+    vim.api.nvim_buf_get_name(0)
+  end,
 }
 lint.linters.eslint.ignore_exitcode = true
+
+-- Ruff configuration
+lint.linters.ruff.args = {
+  "check",
+  "--ignore", "F706",
+  "--builtins", "context,request,container",
+  "--extend-ignore", "F821",
+  "--stdin-filename",
+  function()
+    return vim.api.nvim_buf_get_name(0)
+  end,
+  "--output-format", "json",
+  "-",
+}
+lint.linters.ruff.ignore_exitcode = true
 
 lint.linters_by_ft = {
   javascript = { "eslint" },
@@ -17,9 +35,17 @@ lint.linters_by_ft = {
   sql = { "sqlfluff" },
 }
 
+-- Diagnostic settings to actually SEE the errors
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
 -- Autolint on certain events
 local lint_augroup = vim.api.nvim_create_augroup("lint_group", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
   group = lint_augroup,
   callback = function()
     lint.try_lint()
