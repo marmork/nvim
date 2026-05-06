40c43accd410d645bf08f272433f30331383f36b
Marcel Mork <marcel.mork@perfact.de>
2026-05-06 21:18:41 +0200

    Add configuration of claude sandbox

--- lua/config/local_paths-example.lua
+++ lua/config/local_paths-example.lua
@@ -2,5 +2,6 @@ return {
     coding_path = '',  -- path to coding repos, e.g. ~/repos
     writing_path = '',  -- path to writing documents, e.g.  ~/Documents
     zettelkasten_path = '',  -- zettelkasten path, e.g. ~/Documents/Zettelkasten
-    zotero_bibfile = ''  --full path to zotero CSL/JSON library export
+    zotero_bibfile = ''  -- path to zotero CSL/JSON library export
+    claude_sandbox_path = '' -- path to claude sandbox, requires NVIM_MODE to be set to work in fish config
 }
