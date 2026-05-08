c80802503682d6b2a1f9da660347197be05e2e71
Marcel Mork <marcel.mork@perfact.de>
2026-05-08 11:02:45 +0200

    Remove obsolete configuration option

--- lua/config/local_paths-example.lua
+++ lua/config/local_paths-example.lua
@@ -3,5 +3,4 @@ return {
     writing_path = '',  -- path to writing documents, e.g.  ~/Documents
     zettelkasten_path = '',  -- zettelkasten path, e.g. ~/Documents/Zettelkasten
     zotero_bibfile = ''  -- path to zotero CSL/JSON library export
-    claude_sandbox_path = '' -- path to claude sandbox, requires NVIM_MODE to be set to work in fish config
 }
